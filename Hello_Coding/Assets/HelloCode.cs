90b447653e38967b831a7b2e9beb5df78e26c329
dimpledowon <dowon1102@naver.com>
2022-01-14 12:26:01 +0900

    HelloCode_01
    20220114

---
+++ Hello_Coding/Assets/HelloCode.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloCode : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Hello World!를 콘솔에 출력
+        Debug.Log("Hello World!");
+    }
+}
