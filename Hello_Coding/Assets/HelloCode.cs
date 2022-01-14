37db5f4cf608d22da8a372731206b10f9fdebd49
dimpledowon <dowon1102@naver.com>
2022-01-14 12:30:15 +0900

    Update HelloCode.cs

--- Hello_Coding/Assets/HelloCode.cs
+++ Hello_Coding/Assets/HelloCode.cs
@@ -4,10 +4,8 @@ using UnityEngine;
 
 public class HelloCode : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        // Hello World!를 콘솔에 출력
+    void Start(){
+        // 캐릭터의 프로필을 변수로 만들기
         Debug.Log("Hello World!");
     }
 }
